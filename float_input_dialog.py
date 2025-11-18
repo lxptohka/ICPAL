360492576c6ee872f3037c3387e7448882e6980c
Meliodas <91417559+lxptohka@users.noreply.github.com>
2025-11-18 16:56:53 +0800

    Update float_input_dialog.py

--- float_input_dialog.py
+++ float_input_dialog.py
@@ -7,40 +7,41 @@ class FloatInputDialog(QDialog):
         super().__init__()
         self.setWindowTitle("Set Entropy Threshold")
 
-        # 创建输入提示标签和输入框
+        # Create input label and input field
         label = QLabel("information entropy threshold:")
         self.input_field = QLineEdit()
         self.input_field.setPlaceholderText("for example: 0.5")
 
-        # 设置输入验证器，确保输入为0到1之间的浮点数
+        # Set input validator to ensure the input is a float between 0 and 1
         validator = QDoubleValidator(0.0, 1.0, 2)
         self.input_field.setValidator(validator)
 
-        # 创建按钮
+        # Create buttons
         ok_button = QPushButton("OK")
         cancel_button = QPushButton("Cancel")
 
-        # 连接按钮事件
+        # Connect button events
         ok_button.clicked.connect(self.accept)
         ok_button.setFixedSize(150, 35)
         cancel_button.clicked.connect(self.reject)
         cancel_button.setFixedSize(150, 35)
 
-        # 布局设置
+        # Set main layout
         layout = QVBoxLayout()
         layout.addWidget(label)
         layout.addWidget(self.input_field)
 
-        # 按钮布局
+        # Button layout
         button_layout = QHBoxLayout()
         button_layout.addWidget(ok_button)
         button_layout.addWidget(cancel_button)
 
-        # 将按钮布局添加到主布局
+        # Add button layout to main layout
         layout.addLayout(button_layout)
         self.setLayout(layout)
 
     def get_value(self):
         return self.input_field.text()
+
 
 
